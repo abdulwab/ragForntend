e385350705589e3362a27a34d4189e73917293fb
abdulwab <alvilahori@gmail.com>
2025-07-12 03:26:36 +0500

    Remove background color from main container for a cleaner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
+    <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar
         onNewChat={handleNewChat}
